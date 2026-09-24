169ee585de03b6b9f824583bdb8d9d3184976f0d
87 <dev@87flowers.com>
2026-09-24 21:46:16 +0200

    Bench: 7329461

--- src/thorn/Search.zig
+++ src/thorn/Search.zig
@@ -152,22 +152,26 @@ fn go(self: *Search, out: *std.Io.Writer, ctrl: anytype) !void {
     iterative_deepening: while (depth < max_depth) : (depth += 1) {
         var alpha = -score.infinity;
         var beta = score.infinity;
+        var delta: Score = 25;
         var s = score.none;
 
         if (depth >= 4) {
-            alpha = last_score - 25;
-            beta = last_score + 25;
+            alpha = last_score - delta;
+            beta = last_score + delta;
         }
 
         while (true) {
             s = self.searchRoot(ctrl, alpha, beta, @intCast(depth)) catch break :iterative_deepening;
 
-            if (s <= alpha or s >= beta) {
-                alpha = -score.infinity;
-                beta = score.infinity;
+            if (s <= alpha) {
+                alpha = @max(s - delta, -score.infinity);
+            } else if (s >= beta) {
+                beta = @min(s + delta, score.infinity);
             } else {
                 break;
             }
+
+            delta += delta;
         }
 
         if (self.stopping.load(.monotonic)) break;
